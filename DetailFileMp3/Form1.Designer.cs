ceb828f51775a85d1d16ebfb3b0646f2541abd45
hieuht300894 <30399541+hieuht300894@users.noreply.github.com>
2017-10-27 22:26:05 +0700

    hieuht300894

---
+++ DetailFileMp3/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DetailFileMp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.lbTotal = new System.Windows.Forms.Label();
+            this.pgPercent = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(497, 12);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(12, 12);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(479, 355);
+            this.lstFiles.TabIndex = 1;
+            // 
+            // lbTotal
+            // 
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.Location = new System.Drawing.Point(369, 42);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(0, 13);
+            this.lbTotal.TabIndex = 2;
+            // 
+            // pgPercent
+            // 
+            this.pgPercent.Location = new System.Drawing.Point(12, 376);
+            this.pgPercent.Name = "pgPercent";
+            this.pgPercent.Size = new System.Drawing.Size(560, 23);
+            this.pgPercent.Step = 1;
+            this.pgPercent.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.pgPercent);
+            this.Controls.Add(this.lbTotal);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Label lbTotal;
+        private System.Windows.Forms.ProgressBar pgPercent;
+    }
+}
+
---
+++ DetailFileMp3/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DetailFileMp3
+{
+    public partial class Form1 : Form
+    {
+        List<string> lstFileNames = new List<string>();
+        List<string> lstExtension = new List<string>() { "mp3", "flac", "m4a" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            //string fileName= @"E:\Musics\You And You Only Chipmunk.mp3";
+            //TagLib.File file = TagLib.File.Create(fileName);
+            //file.Tag.Title = Path.GetFileNameWithoutExtension(fileName);
+            //file.Save();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            lstFileNames = new List<string>();
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = dialog.SelectedPath;
+                if (File.GetAttributes(path) == FileAttributes.Directory)
+                {
+                    string[] fileNames = Directory.GetFiles(path);
+                    string[] pathNames = Directory.GetDirectories(path);
+                    foreach (string pathName in pathNames)
+                    {
+                        if (Directory.Exists(pathName))
+                            GetFiles(pathName);
+                    }
+                    foreach (string fileName in fileNames)
+                    {
+                        string extension = Path.GetExtension(fileName).TrimStart('.');
+                        if (File.Exists(fileName) && lstExtension.Any(x => x.Equals(extension.ToLower())))
+                            lstFileNames.Add(fileName);
+                    }
+                }
+            }
+            lstFiles.Items.AddRange(lstFileNames.ToArray());
+            lbTotal.Text = lstFileNames.Count.ToString();
+
+            ChangeTitle();
+        }
+        private void GetFiles(string path)
+        {
+            string[] fileNames = Directory.GetFiles(path);
+            string[] pathNames = Directory.GetDirectories(path);
+            foreach (string pathName in pathNames)
+            {
+                if (Directory.Exists(pathName))
+                    GetFiles(pathName);
+            }
+            foreach (string fileName in fileNames)
+            {
+                string extension = Path.GetExtension(fileName).TrimStart('.');
+                if (File.Exists(fileName) && lstExtension.Any(x => x.Equals(extension.ToLower())))
+                    lstFileNames.Add(fileName);
+            }
+        }
+
+        private void ChangeTitle()
+        {
+            BackgroundWorker bWorker = new BackgroundWorker() { WorkerReportsProgress = true };
+            Timer timer = new Timer() { Interval = 250 };
+            bool IsComplete = false;
+            int Total = lstFileNames.Count;
+            int Current = 0;
+            pgPercent.Value = 0;
+
+            timer.Tick += (sender, e) =>
+            {
+                if (!IsComplete)
+                {
+                    bWorker.ReportProgress(Convert.ToInt32(((Current * 1.0f) / Total) * 100));
+                }
+            };
+            bWorker.DoWork += (sender, e) =>
+            {
+                foreach (string fileName in lstFileNames)
+                {
+                    TagLib.File file = TagLib.File.Create(fileName);
+                    file.Tag.Title = Path.GetFileNameWithoutExtension(fileName);
+                    file.Save();
+                    Current++;
+                }
+                IsComplete = true;
+            };
+            bWorker.ProgressChanged += (sender, e) =>
+            {
+                if (pgPercent.InvokeRequired)
+                {
+                    Action<int> action = (val) => { pgPercent.Value = val; };
+                    pgPercent.Invoke(action, e.ProgressPercentage);
+                }
+                else
+                {
+                    pgPercent.Value = e.ProgressPercentage;
+                }
+            };
+            bWorker.RunWorkerCompleted += (sender, e) =>
+            {
+                if (pgPercent.InvokeRequired)
+                {
+                    Action action = () => { pgPercent.Visible = false; };
+                    pgPercent.Invoke(action);
+                }
+                else
+                {
+                    pgPercent.Visible = false;
+                }
+
+                timer.Enabled = false;
+                timer.Dispose();
+                bWorker.Dispose();
+            };
+
+            if (Total > 0)
+            {
+                pgPercent.Visible = true;
+                bWorker.RunWorkerAsync();
+                timer.Enabled = true;
+            }
+        }
+    }
+}
